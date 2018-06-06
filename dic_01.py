1abbf532e76ae50f3eb48ef8620ad3399f5e25b3
shanhuazun.zqgame <huazun.shan@zqgame.com>
2018-06-06 16:19:11 +0800

    项目本地测试03

---
+++ dic_01.py
@@ -0,0 +1,65 @@
+dic = {"a":1}
+for k in dic:
+    print k
+for k in dic:
+    print dic[k]
+a = "abc123"
+a.upper()
+a = "12323434"
+a.upper()
+li = [1, 2, 3]
+''.join(li)
+" ".join(li)
+li = ["a", "b", "c"]
+''.join(li)
+dic = {"a":1}
+for key in dic:
+    print key
+[dic.get(key) for key in dic]
+dic = {"a":1, "b":2}
+dic = {"a":1}
+a = "ABC"
+a.lower()
+def foo():
+    pass
+def abc(x):
+    if x == 1:
+        foo() == True
+    return foo()
+abc(1)
+print abc(1)
+print foo()
+def foo():
+    return True
+def abc(x):
+    if x == 1:
+        foo() == True
+        return foo()
+    else:
+        foo() == False
+        return foo()
+abc(1)
+abc(2)
+def abc(x):
+    if x == 1:
+        flag == 1
+        return foo(flag)
+    else:
+        flag == 0
+        return foo(flag)
+def foo(flag):
+    if flag == 1:
+        return True
+abc(1)
+flag == True
+flag = True
+if flag:
+    print "====="
+flag = False
+if flag:
+    print "====="
+155.5/5
+a = 155.5
+type(a)
+155.5/5
+%hist -f dic_01.py
