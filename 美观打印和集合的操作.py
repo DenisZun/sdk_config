6cf9544cd8a362812569ff5117454adcbf01f52f
shanhuazun.zqgame <huazun.shan@zqgame.com>
2018-06-12 11:40:30 +0800

    美观打印和集合的操作

---
+++ 美观打印和集合的操作.py
@@ -0,0 +1,15 @@
+lis = [1, 2, 3, 4]
+lis - [1]
+lis.pop(i)
+data = (  
+    "this is a string", [1, 2, 3, 4], ("more tuples",  
+    1.0, 2.3, 4.5), "this is yet another string"  
+    )
+print data
+import pprint
+pprint.pprint(data)
+i = "a"
+j = "b"
+k = "c"
+set(i + j + k)
+%hist -f "美观打印和集合的操作.py"
